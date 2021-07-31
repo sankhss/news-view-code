cc45608dfd45813823fd2d207b6b25de27435213
Samuel Silva <samuelh.dev@gmail.com>
2021-07-31 15:32:27 -0300

    add theme manager

---
+++ news/ThemeManager.swift
@@ -0,0 +1,15 @@
+//
+//  ThemeManager.swift
+//  news
+//
+//  Created by Samuel Silva on 31/07/21.
+//
+
+import UIKit
+
+class ThemeManager {
+    
+    static func setup() {
+        UINavigationBar.appearance().barTintColor = UIColor(displayP3Red: 47/255, green: 54/255, blue: 64/255, alpha: 1.0)
+    }
+}
