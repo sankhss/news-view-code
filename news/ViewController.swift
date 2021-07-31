be5ffe307c8ab81d5d6e334e8ae85d93ff322baa
Samuel Silva <samuelh.dev@gmail.com>
2021-07-31 14:57:29 -0300

    initial project setup

--- news/ViewController.swift
+++ news/ViewController.swift
@@ -8,12 +8,15 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    override func loadView() {
+        let view = UIView(frame: UIScreen.main.bounds)
+        view.backgroundColor = .gray
+        self.view = view
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
 
